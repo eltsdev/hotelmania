b284bce7ffd6394ef997443983ba38b8c63a4d12
lorencr7 <eden__lorenzo@hotmail.com>
2014-04-27 15:57:28 +0200

    Añadido metodo para enviar una request

--- simulation/src/hotelmania/group2/platform/AgentsFinder.java
+++ simulation/src/hotelmania/group2/platform/AgentsFinder.java
@@ -1,10 +1,20 @@
 package hotelmania.group2.platform;
 
+import javax.media.j3d.Behavior;
+
+import jade.content.Concept;
+import jade.content.lang.Codec;
+import jade.content.lang.Codec.CodecException;
+import jade.content.onto.Ontology;
+import jade.content.onto.OntologyException;
+import jade.content.onto.basic.Action;
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 
 public class AgentsFinder {
 	
@@ -29,100 +39,35 @@ public class AgentsFinder {
 			if (agents != null && agents.length > 0) {
 
 				for (DFAgentDescription description : agents) {
-					//bankFound = true;
 					return description.getName(); // only expects 1
 					// agent...
 				}
 			}
 		} catch (Exception e) {
-			//bankFound = false;
+			return null;
 		}
 		return null;
-
 	}
 	
-	/**
-	 * 
-	 */
-	/*public AID locateBank(String type, Agent myAgent) {
-		DFAgentDescription dfd = new DFAgentDescription();
-		ServiceDescription sd = new ServiceDescription();
-		sd.setType(type);
-		dfd.addServices(sd);
-
+	public void sendRequest (Agent sender, AID receiver, Concept concept, Codec codec, Ontology ontology) {
+		ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+		msg.addReceiver(receiver);
+		msg.setLanguage(codec.getName());
+		msg.setOntology(ontology.getName());
+		// As it is an action and the encoding language the SL,
+		// it must be wrapped into an Action
+		Action agAction = new Action(receiver, concept);
 		try {
-			// It finds agents of the required type
-			DFAgentDescription[] agents = DFService.search(myAgent, dfd);
-
-			if (agents != null && agents.length > 0) {
-
-				for (DFAgentDescription description : agents) {
-					//bankFound = true;
-					return description.getName(); // only expects 1
-					// agent...
-				}
-			}
-		} catch (Exception e) {
-			//bankFound = false;
+			// The ContentManager transforms the java objects into strings
+			sender.getContentManager().fillContent(msg, agAction);
+			sender.send(msg);
+			//
+		} catch (CodecException ce) {
+			ce.printStackTrace();
+		} catch (OntologyException oe) {
+			oe.printStackTrace();
 		}
-		return null;
 
-	}*/
-
-	/**
-	 * @return
-	 * 
-	 * 
-	 */
-	/*public AID locateHotel(String type, Agent myAgent) {
-		DFAgentDescription dfd = new DFAgentDescription();
-		ServiceDescription sd = new ServiceDescription();
-		sd.setType(type);
-		dfd.addServices(sd);
-
-		try {
-			// It finds agents of the required type
-			DFAgentDescription[] agents = DFService.search(myAgent, dfd);
-
-			if (agents != null && agents.length > 0) {
-
-				for (DFAgentDescription description : agents) {
-					//hotelFound = true;
-					return description.getName(); // only expects 1
-					// agent...
-				}
-			}
-		} catch (Exception e) {
-			//hotelFound = false;
-		}
-		return null;
-
-	}*/
-
-	/**
-	 * @return
-	 */
-	/*public AID locateHotelMania(String type, Agent myAgent) {
-		DFAgentDescription dfd = new DFAgentDescription();
-		ServiceDescription sd = new ServiceDescription();
-		sd.setType(type);
-		dfd.addServices(sd);
-
-		try {
-			// It finds agents of the required type
-			DFAgentDescription[] agents = DFService.search(myAgent, dfd);
-
-			if (agents != null && agents.length > 0) {
-
-				for (DFAgentDescription description : agents) {
-					return (AID) description.getName(); // only expects 1
-														// agent...
-				}
-			}
-		} catch (Exception e) {
-		}
-		return null;
-
-	}*/
+	}
 
 }
