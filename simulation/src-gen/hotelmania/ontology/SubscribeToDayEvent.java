59686e8743f7c7ae5047a5d841c80174203f466a
Leonardo Tellez <leonardo.tellezs@gmail.com>
2014-05-05 12:29:33 +0200

    added

---
+++ simulation/src-gen/hotelmania/ontology/SubscribeToDayEvent.java
@@ -0,0 +1,15 @@
+package hotelmania.ontology;
+
+
+import jade.content.*;
+import jade.util.leap.*;
+import jade.core.*;
+
+/**
+* Protege name: SubscribeToDayEvent
+* @author ontology bean generator
+* @version 2014/05/5, 12:27:36
+*/
+public class SubscribeToDayEvent implements AgentAction {
+
+}
