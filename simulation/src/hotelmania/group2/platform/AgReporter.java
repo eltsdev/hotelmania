d864777ff7275a6b014d740e984f8823f39261d0
Leonardo Tellez <leonardo.tellezs@gmail.com>
2014-05-23 18:15:42 +0200

    Skeleton for simulation report.

--- simulation/src/hotelmania/group2/platform/AgReporter.java
+++ simulation/src/hotelmania/group2/platform/AgReporter.java
@@ -66,27 +66,32 @@ public class AgReporter extends MetaAgent
 			super(a);
 		}
 
+		@Override
 		public void action() {
-			block();
-//			FIXME askForReportData();
-//			generateSimulationReport();	
-			setDone(true);
+			if (agHotelmania == null) {
+				agHotelmania = locateAgent(Constants.CONSULTHOTELSINFO_ACTION, myAgent);
+			} else {
+				this.consultHotelInfo();
+				this.setDone(true);
+			}
+
+			if (agBank== null) {
+				agBank = locateAgent(Constants.CONSULTFINANCEREPORT, myAgent);
+			} else {
+				this.consultFinanceReport();
+				this.setDone(true);
+			}
 		}
 
-
-		public void askForReportData() {
-		QueryHotelmaniaHotel consult_request = new QueryHotelmaniaHotel();
-		sendRequest(agHotelmania, consult_request,
-				Constants.CONSULTHOTELSINFO_PROTOCOL, ACLMessage.QUERY_REF);
+		public void consultHotelInfo() {
+			QueryHotelmaniaHotel consult_request = new QueryHotelmaniaHotel();
+			sendRequest(agHotelmania, consult_request,
+					Constants.CONSULTHOTELSINFO_PROTOCOL, ACLMessage.QUERY_REF);
 		}
 
+		private void consultFinanceReport() {
+			// TODO Auto-generated method stub
 
-		public void generateSimulationReport() {
-			System.out.println("==================================================================");
-			System.out.println("SIMULATION RESULTS");
-			System.out.println("==================================================================");
-			
-			
 		}
 
 	}
@@ -116,5 +121,25 @@ public class AgReporter extends MetaAgent
 	public void receivedInform(ACLMessage message) {
 		// TODO Auto-generated method stub
 		
+		if (message.getProtocol().equals(Constants.CONSULTFINANCEREPORT_PROTOCOL)) {
+			//TODO
+			generateSimulationReport();
+		}
+		
+		else if (message.getProtocol().equals(Constants.CONSULTHOTELSINFO_PROTOCOL)) {
+			//TODO
+			generateSimulationReport();
+		}
+		
 	}
+	
+	public void generateSimulationReport() {
+		System.out.println("==================================================================");
+		System.out.println("SIMULATION RESULTS");
+		System.out.println("==================================================================");
+		
+		
+	}
+
+
 }
