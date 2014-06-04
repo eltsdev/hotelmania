308ba8784b62dd2974337d2b544c865aee1ce8f1
Guillermo Aranda <guillealk@gmail.com>
2014-06-04 12:04:58 +0200

    Adding class.

---
+++ simulation/src/hotelmania/group2/behaviours/GenericServerBehaviour.java
@@ -0,0 +1,51 @@
+package hotelmania.group2.behaviours;
+
+import hotelmania.group2.platform.AbstractAgent;
+import hotelmania.group2.platform.MetaSimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+
+public abstract class GenericServerBehaviour extends MetaSimpleBehaviour {
+	
+	protected static final long serialVersionUID = -4878507137076376248L;
+	protected AbstractAgent myAgent;
+	protected String protocol;
+	protected MessageTemplate messageTemplate;
+
+	public GenericServerBehaviour(AbstractAgent myAgent, String protocol) {
+		super(myAgent);
+		this.myAgent = myAgent;
+		this.protocol = protocol;
+		
+		this.messageTemplate = MessageTemplate.and(MessageTemplate.and(
+				MessageTemplate.MatchLanguage(myAgent.getCodec().getName()),
+				MessageTemplate.MatchOntology(myAgent.getOntology().getName())),
+				MessageTemplate.MatchProtocol(this.protocol));
+	}
+	
+	@Override
+	public void action() {
+		ACLMessage msg = myAgent.receive(messageTemplate);
+		if (msg == null) { 	
+			// No message, so wait for new ones
+			block();					
+		}
+		else {
+			//A New Message
+			myAgent.getLog().logReceivedMsg(msg);
+			
+			ACLMessage reply = doSendResponse(msg);
+			myAgent.send(reply);
+			myAgent.getLog().logSendReply(reply);
+		}
+	}
+
+	/**
+	 * Placeholder for sending the message to the targe agent (one or more messages to one or more agents, etc).  
+	 * @param msg 
+	 * @return 
+	 */
+	protected abstract ACLMessage doSendResponse(ACLMessage msg);
+	
+}
---
+++ simulation/src/hotelmania/group2/behaviours/ServerStep.java
@@ -0,0 +1,8 @@
+package hotelmania.group2.behaviours;
+
+/**
+ * The steps of a generic receive-response behavior
+ */
+public enum ServerStep {
+	RECEIVE_REQUESTS, SEND_RESPONSE, DONE;
+}
