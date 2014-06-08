9e622c7f151966ddb407a3a62783fec2596b9661
Leonardo Tellez <leonardo.tellezs@gmail.com>
2014-06-08 10:13:44 +0200

    (cosmetic change for documentation)

--- simulation/src/hotelmania/group2/platform/AbstractAgent.java
+++ simulation/src/hotelmania/group2/platform/AbstractAgent.java
@@ -50,6 +50,12 @@ public abstract class AbstractAgent extends Agent implements IMyName {
 	
 	private Integer day;
 
+	public String myName()
+	{
+		return super.getLocalName();
+	}
+
+
 	@Override
 	protected void setup() {
 		super.setup();
@@ -62,32 +68,34 @@ public abstract class AbstractAgent extends Agent implements IMyName {
 		getContentManager().registerOntology(this.ontology);
 
 		//Behaviors
-		addBehaviour(new LocateSimulatorBehavior(this));
+		addBehaviour(new SubscribeToSimulatorEventsBehavior(this));
 		
 		addBehaviour(new ReceiveMsgOfSubscriptionsBehavior(this));
 
 	}
 
 	
-	private void loadProperties() {
-		AgPlatform2.loadProperties();
-		
-	}
+	/**
+	 * Override to set the registration to day events.  
+	 * @return
+	 */
+	protected abstract boolean setRegisterForDayEvents();
 
-	public String myName()
+
+	protected boolean setRegisterForEndSimulation()
 	{
-		return super.getLocalName();
+		return true;
 	}
 
-	final class LocateSimulatorBehavior extends MetaSimpleBehaviour {
-
+	final class SubscribeToSimulatorEventsBehavior extends MetaSimpleBehaviour {
+	
 		private static final long serialVersionUID = 9034688687283651380L;
 		private AID agSimulator;
-
-		public LocateSimulatorBehavior(Agent a) {
+	
+		public SubscribeToSimulatorEventsBehavior(Agent a) {
 			super(a);
 		}
-
+	
 		@Override
 		public void action() {
 			if (agSimulator == null) {
@@ -97,33 +105,255 @@ public abstract class AbstractAgent extends Agent implements IMyName {
 				{
 					addSubscriptionToDayEventBehavior(agSimulator);
 				}
-
+	
 				if (setRegisterForEndSimulation()) {
 					addSubscriptionToEndSimulation(agSimulator);
 				}
 				this.setDone(true);
 			}
 		}
+	
+		private void addSubscriptionToDayEventBehavior(AID agSimulator) {
+		
+			if (agSimulator == null) {
+				return;
+			}
+		
+			ACLMessage msg = new ACLMessage(ACLMessage.SUBSCRIBE);
+			msg.addReceiver(agSimulator);
+			msg.setProtocol(Constants.SUBSCRIBETODAYEVENT_PROTOCOL);
+			msg.setLanguage(codec.getName());
+			msg.setOntology(ontology.getName());
+			msg.setReplyWith(ConversationID.newCID());
+		
+			SubscribeToDayEvent subscribeToDayEvent = new SubscribeToDayEvent();
+			Action action = new Action(agSimulator, subscribeToDayEvent);
+			try {
+				getContentManager().fillContent(msg, action);
+			} catch (CodecException e) {
+				e.printStackTrace();
+			} catch (OntologyException e) {
+				e.printStackTrace();
+			}
+		
+			addBehaviour(new DayEventSubscriptor(myAgent, msg));
+			log.logSendRequest(msg);
+		}
+	
+		private void addSubscriptionToEndSimulation(AID agSimulator) {
+		
+			if (agSimulator == null) {
+				return;
+			}
+		
+			ACLMessage msg = new ACLMessage(ACLMessage.SUBSCRIBE);
+			msg.addReceiver(agSimulator);
+			msg.setProtocol(Constants.END_SIMULATION_PROTOCOL);
+			msg.setLanguage(codec.getName());
+			msg.setOntology(ontology.getName());
+			msg.setReplyWith(ConversationID.newCID());
+		
+			EndSimulation actionEndSim = new EndSimulation(); 
+			Action action = new Action(agSimulator, actionEndSim);
+			try {
+				getContentManager().fillContent(msg, action);
+			} catch (CodecException e) {
+				e.printStackTrace();
+			} catch (OntologyException e) {
+				e.printStackTrace();
+			}
+		
+			addBehaviour(new EndSimulationSubscriptor(myAgent, msg));
+			log.logSendRequest(msg);
+		}
 	}
 
-	/**
-	 * Override to set the registration to day events.  
-	 * @return
-	 */
-	protected abstract boolean setRegisterForDayEvents();
-
-	protected boolean setRegisterForEndSimulation()
-	{
-		return true;
+	private final class ReceiveMsgOfSubscriptionsBehavior extends CyclicBehaviour {
+	
+		private static final long serialVersionUID = -4878774871721189228L;
+	
+		MessageTemplate informTemplate = MessageTemplate.and(BASIC_TEMPLATE,
+			MessageTemplate.or(MessageTemplate.MatchProtocol(Constants.SUBSCRIBETODAYEVENT_PROTOCOL),MessageTemplate.MatchProtocol(Constants.END_SIMULATION_PROTOCOL)));
+	
+		private ReceiveMsgOfSubscriptionsBehavior(AbstractAgent a) {
+			super(a);
+		}
+	
+		public void action() {
+			ACLMessage msg = receive(informTemplate);
+	
+			if (msg != null) {
+				
+				log.logReceivedMsg(msg);
+				
+				switch ( msg.getPerformative() ) {
+				case ACLMessage.AGREE:
+					return;
+				case ACLMessage.INFORM:
+					if (msg.getProtocol().equals(Constants.SUBSCRIBETODAYEVENT_PROTOCOL)) {
+						handleInformNewDay(msg);
+						return;
+					} else if(msg.getProtocol().equals(Constants.END_SIMULATION_PROTOCOL)) {
+						handleInformEndSimulation(msg);
+						return;
+					}
+	
+					break;
+	
+				default:
+					break;
+				}
+				Logger.logError("Message received but not expected: " +msg.toString());
+	
+	
+			} else {
+				// If no message arrives
+				block();
+			}
+	
+		}
 	}
+
+	private final class DayEventSubscriptor extends SubscriptionInitiator {
+	
+		private static final long serialVersionUID = 1L;
+	
+		private DayEventSubscriptor(Agent a, ACLMessage msg) {
+			super(a, msg);
+		}
+	
+		@Override
+		protected void handleAgree(ACLMessage agree) {
+			getLog().logAgreeMessage(agree);
+			state.check(State.DAYEVENT_SUBSCRIBED);
+		}
+	
+		protected void handleInform(ACLMessage inform) {
+			handleInformNewDay(inform);
+		}
+	
+		protected void handleRefuse(ACLMessage refuse) {
+			getLog().logRefuseMessage(refuse);
+			state.uncheck(State.DAYEVENT_SUBSCRIBED);
+		}
+	
+		protected void handleFailure(ACLMessage failure) {
+			/*TODO refine message logs
+				if (failure.getSender().equals(myAgent.getAMS())) {
+					// FAILURE notification from the JADE runtime: 
+					// the receiver does not exist
+					Logger.logDebug("Responder does not exist");
+				} else {
+					Logger.logDebug("failed to perform the requested action");
+				}
+			 */
+			getLog().logFailureMessage(failure);
+			state.uncheck(State.DAYEVENT_SUBSCRIBED);
+		}
+	}
+
+	public void handleInformNewDay(ACLMessage inform) {
+		getLog().logInformMessage(inform);
+		NotificationDayEvent event = (NotificationDayEvent) getPredicateFromMessage(inform);
+		this.day = event.getDayEvent().getDay();
+		if (this.day > Constants.DAY ) {
+			Constants.DAY = day;
+		}
+		doOnNewDay();
+		
+	}
+
+
+	public int getDay() {
+		return Constants.DAY;
+	}
+
 
 	protected void doOnNewDay() {
 		state.check(State.RECEIVED_DAY_NOTIFICATION);
 	}
 
-	public int getDay() {
-		return Constants.DAY;
+	private final class EndSimulationSubscriptor extends SubscriptionInitiator {
+	
+		private static final long serialVersionUID = 1L;
+	
+		private EndSimulationSubscriptor(Agent a, ACLMessage msg) {
+			super(a, msg);
+		}
+	
+		@Override
+		protected void handleAgree(ACLMessage agree) {
+			state.check(State.ENDSIMULATION_SUBSCRIBED);
+			getLog().logAgreeMessage(agree);
+			super.handleAgree(agree);
+		}
+	
+		protected void handleInform(ACLMessage inform) {
+			handleInformEndSimulation(inform);
+		}
+	
+		protected void handleRefuse(ACLMessage refuse) {
+			state.uncheck(State.ENDSIMULATION_SUBSCRIBED);
+			getLog().logRefuseMessage(refuse);
+		}
+	
+		protected void handleFailure(ACLMessage failure) {
+			state.uncheck(State.ENDSIMULATION_SUBSCRIBED);
+			getLog().logFailureMessage(failure);
+		}
 	}
+
+	public void handleInformEndSimulation(ACLMessage inform) {
+		state.check(State.RECEIVED_ENDSIMULATION_NOTIFICATION);
+		getLog().logInformMessage(inform);
+		boolean die = doBeforeDie();
+		if (die) {
+			doDelete();
+			Logger.logDebug(myName()+": DIED");
+		}else {
+			Logger.logDebug(myName()+": RESISTED TO DIE");
+		}
+	}
+
+
+	/**
+	 * Invoked just before the agent dies
+	 * @return die true if this agent shall die immediately. False if the agent needs to keep alive.
+	 */
+	public boolean doBeforeDie() {
+		return true;
+	}
+	
+	//---------------------------------------------------
+	// Utilities for message handling
+	//---------------------------------------------------
+
+	private void loadProperties() {
+		AgPlatform2.loadProperties();
+		
+	}
+
+
+	public void registerServices(String...services) {
+		DFAgentDescription dfd = new DFAgentDescription();
+		for (int i = 0; i < services.length; i++) {
+			ServiceDescription registrationService = new ServiceDescription();
+			registrationService.setName(this.getName());
+			registrationService.setType(services[i]);
+			dfd.addServices(registrationService);
+		}
+	
+		try {	
+			// Registers its description in the DF
+			DFService.register(this, dfd);
+			Logger.logDebug(myName() + ": registered in the DF");
+			dfd = null;
+		} catch (FIPAException e) {
+			// TODO handle
+			e.printStackTrace();
+		}
+	}
+
 
 	public AID locateAgent(String type, Agent myAgent) {
 
@@ -206,178 +436,24 @@ public abstract class AbstractAgent extends Agent implements IMyName {
 		msg.setOntology(this.ontology.getName());
 		msg.setProtocol(protocol);
 		msg.setReplyWith(ConversationID.newCID());
-
+	
 		this.send(msg);
 		log.logSendRequest(msg);
 	}
 
-	public void registerServices(String...services) {
-		DFAgentDescription dfd = new DFAgentDescription();
-		for (int i = 0; i < services.length; i++) {
-			ServiceDescription registrationService = new ServiceDescription();
-			registrationService.setName(this.getName());
-			registrationService.setType(services[i]);
-			dfd.addServices(registrationService);
-		}
 
-		try {	
-			// Registers its description in the DF
-			DFService.register(this, dfd);
-			Logger.logDebug(myName() + ": registered in the DF");
-			dfd = null;
-		} catch (FIPAException e) {
-			// TODO handle
-			e.printStackTrace();
-		}
-	}
-
-	private void addSubscriptionToDayEventBehavior(AID agSimulator) {
-
-		if (agSimulator == null) {
-			return;
-		}
-
-		ACLMessage msg = new ACLMessage(ACLMessage.SUBSCRIBE);
-		msg.addReceiver(agSimulator);
-		msg.setProtocol(Constants.SUBSCRIBETODAYEVENT_PROTOCOL);
+	public void sendRequestEmpty(AID receiver, String protocol, int performative) {
+		ACLMessage msg = new ACLMessage(performative);
+		msg.addReceiver(receiver);
 		msg.setLanguage(this.codec.getName());
 		msg.setOntology(this.ontology.getName());
+		msg.setProtocol(protocol);
 		msg.setReplyWith(ConversationID.newCID());
-
-		SubscribeToDayEvent subscribeToDayEvent = new SubscribeToDayEvent();
-		Action action = new Action(agSimulator, subscribeToDayEvent);
-		try {
-			this.getContentManager().fillContent(msg, action);
-		} catch (CodecException e) {
-			e.printStackTrace();
-		} catch (OntologyException e) {
-			e.printStackTrace();
-		}
-
-		addBehaviour(new DayEventSubscriptor(this, msg));
-		log.logSendRequest(msg);
-	}
-
-	private void addSubscriptionToEndSimulation(AID agSimulator) {
-
-		if (agSimulator == null) {
-			return;
-		}
-
-		ACLMessage msg = new ACLMessage(ACLMessage.SUBSCRIBE);
-		msg.addReceiver(agSimulator);
-		msg.setProtocol(Constants.END_SIMULATION_PROTOCOL);
-		msg.setLanguage(this.codec.getName());
-		msg.setOntology(this.ontology.getName());
-		msg.setReplyWith(ConversationID.newCID());
-
-		EndSimulation actionEndSim = new EndSimulation(); 
-		Action action = new Action(agSimulator, actionEndSim);
-		try {
-			this.getContentManager().fillContent(msg, action);
-		} catch (CodecException e) {
-			e.printStackTrace();
-		} catch (OntologyException e) {
-			e.printStackTrace();
-		}
-
-		addBehaviour(new EndSimulationSubscriptor(this, msg));
-		log.logSendRequest(msg);
-	}
-
-
-	private final class DayEventSubscriptor extends SubscriptionInitiator {
-
-		private static final long serialVersionUID = 1L;
-
-		private DayEventSubscriptor(Agent a, ACLMessage msg) {
-			super(a, msg);
-		}
-
-		@Override
-		protected void handleAgree(ACLMessage agree) {
-			getLog().logAgreeMessage(agree);
-			state.check(State.DAYEVENT_SUBSCRIBED);
-		}
-
-		protected void handleInform(ACLMessage inform) {
-			handleInformNewDay(inform);
-		}
-
-		protected void handleRefuse(ACLMessage refuse) {
-			getLog().logRefuseMessage(refuse);
-			state.uncheck(State.DAYEVENT_SUBSCRIBED);
-		}
-
-		protected void handleFailure(ACLMessage failure) {
-			/*TODO refine message logs
-				if (failure.getSender().equals(myAgent.getAMS())) {
-					// FAILURE notification from the JADE runtime: 
-					// the receiver does not exist
-					Logger.logDebug("Responder does not exist");
-				} else {
-					Logger.logDebug("failed to perform the requested action");
-				}
-			 */
-			getLog().logFailureMessage(failure);
-			state.uncheck(State.DAYEVENT_SUBSCRIBED);
-		}
-	}
-
-	private final class EndSimulationSubscriptor extends SubscriptionInitiator {
-
-		private static final long serialVersionUID = 1L;
-
-		private EndSimulationSubscriptor(Agent a, ACLMessage msg) {
-			super(a, msg);
-		}
-
-		@Override
-		protected void handleAgree(ACLMessage agree) {
-			state.check(State.ENDSIMULATION_SUBSCRIBED);
-			getLog().logAgreeMessage(agree);
-			super.handleAgree(agree);
-		}
-
-		protected void handleInform(ACLMessage inform) {
-			handleInformEndSimulation(inform);
-		}
-
-		protected void handleRefuse(ACLMessage refuse) {
-			state.uncheck(State.ENDSIMULATION_SUBSCRIBED);
-			getLog().logRefuseMessage(refuse);
-		}
-
-		protected void handleFailure(ACLMessage failure) {
-			state.uncheck(State.ENDSIMULATION_SUBSCRIBED);
-			getLog().logFailureMessage(failure);
-		}
-	}
-
-	/**
-	 * Invoked just before the agent dies
-	 * @return die true if this agent shall die immediately. False if the agent needs to keep alive.
-	 */
-	public boolean doBeforeDie() {
-		return true;
-	}
-
-	public Codec getCodec() {
-		return codec;
-	}
-
-	public Ontology getOntology() {
-		return ontology;
-	}
-
-	public Logger getLog() {
-		return log;
-	}
-
-	public void setLog(Logger log) {
-		this.log = log;
-	}
 	
+		this.send(msg);
+	}
+
+
 	public Concept getConceptFromMessage(ACLMessage msg) {
 		ContentElement ce;
 		try {
@@ -410,84 +486,23 @@ public abstract class AbstractAgent extends Agent implements IMyName {
 		return null;
 	}
 	
-	public void sendRequestEmpty(AID receiver, String protocol, int performative) {
-		ACLMessage msg = new ACLMessage(performative);
-		msg.addReceiver(receiver);
-		msg.setLanguage(this.codec.getName());
-		msg.setOntology(this.ontology.getName());
-		msg.setProtocol(protocol);
-		msg.setReplyWith(ConversationID.newCID());
-
-		this.send(msg);
-	}
-	
-	private final class ReceiveMsgOfSubscriptionsBehavior extends CyclicBehaviour {
-
-		private static final long serialVersionUID = -4878774871721189228L;
-
-		MessageTemplate informTemplate = MessageTemplate.and(BASIC_TEMPLATE,
-			MessageTemplate.or(MessageTemplate.MatchProtocol(Constants.SUBSCRIBETODAYEVENT_PROTOCOL),MessageTemplate.MatchProtocol(Constants.END_SIMULATION_PROTOCOL)));
-
-		private ReceiveMsgOfSubscriptionsBehavior(AbstractAgent a) {
-			super(a);
-		}
-
-		public void action() {
-			ACLMessage msg = receive(informTemplate);
-
-			if (msg != null) {
-				
-				log.logReceivedMsg(msg);
-				
-				switch ( msg.getPerformative() ) {
-				case ACLMessage.AGREE:
-					return;
-				case ACLMessage.INFORM:
-					if (msg.getProtocol().equals(Constants.SUBSCRIBETODAYEVENT_PROTOCOL)) {
-						handleInformNewDay(msg);
-						return;
-					} else if(msg.getProtocol().equals(Constants.END_SIMULATION_PROTOCOL)) {
-						handleInformEndSimulation(msg);
-						return;
-					}
-
-					break;
-
-				default:
-					break;
-				}
-				Logger.logError("Message received but not expected: " +msg.toString());
-
-
-			} else {
-				// If no message arrives
-				block();
-			}
-
-		}
+	public Codec getCodec() {
+		return codec;
 	}
 
-	public void handleInformNewDay(ACLMessage inform) {
-		getLog().logInformMessage(inform);
-		NotificationDayEvent event = (NotificationDayEvent) getPredicateFromMessage(inform);
-		this.day = event.getDayEvent().getDay();
-		if (this.day > Constants.DAY ) {
-			Constants.DAY = day;
-		}
-		doOnNewDay();
-		
+
+	public Ontology getOntology() {
+		return ontology;
 	}
 
-	public void handleInformEndSimulation(ACLMessage inform) {
-		state.check(State.RECEIVED_ENDSIMULATION_NOTIFICATION);
-		getLog().logInformMessage(inform);
-		boolean die = doBeforeDie();
-		if (die) {
-			doDelete();
-			Logger.logDebug(myName()+": DIED");
-		}else {
-			Logger.logDebug(myName()+": RESISTED TO DIE");
-		}
+
+	public Logger getLog() {
+		return log;
+	}
+
+
+	public void setLog(Logger log) {
+		this.log = log;
 	}
 
 }
